b756f2adf82f16deeb761ad077fc4d4c845d0b09
罗世豪 <shawnluoi@outlook.com>
2021-08-28 21:39:53 +0800

    8/28提交

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
+// 按需导入elui组件
 import { Button } from 'element-ui'
+import { Form, FormItem } from 'element-ui'
+import { Input } from 'element-ui'
 
+// use全局注册组件
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
